5aabe4631ebac669e702ebabfe0cb08aa1ec4d2b
Nierusuki <gabrielfidsouza@gmail.com>
2020-04-05 15:23:23 -0300

    Creating board and position

---
+++ Chess-Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Board;
+namespace Chess_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position pos1 = new Position(3, 4);
+            Console.WriteLine("Position: " + pos1);
+        }
+    }
+}
